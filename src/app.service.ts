c23452fae2235c2f8545cf4992d7a16119f8eefa
Halyna Urbanska <aquilla@ukr.net>
2022-11-04 18:36:18 +0200

    store data to InMemory storage

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,29 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PubSubService } from './providers/pubsub/pub-sub.service';
 import util from 'util';
+import { InMemoryStorageService } from './providers/inmemory-storage/in-memory-storage.service';
+import { RideStatus } from './providers/inmemory-storage/enums';
 
 @Injectable()
 export class AppService implements OnModuleInit {
-  constructor(pubSubService: PubSubService) {
+  constructor(
+    private readonly pubSubService: PubSubService,
+    private readonly inMemoryStorage: InMemoryStorageService,
+  ) {
     pubSubService.subscribe(
       (message) => {
         console.log(message);
+        const { ride_id, ride_status, ...move } = message;
+        switch (ride_status) {
+          case RideStatus.DROPOFF:
+            this.inMemoryStorage.finishRide(ride_id);
+            break;
+          case RideStatus.PICKUP:
+            this.inMemoryStorage.setRide(ride_id, move, ride_status);
+            break;
+          case RideStatus.ENROUTE:
+            this.inMemoryStorage.addMoveToRide(ride_id, move, ride_status);
+        }
       },
       (error) => {
         console.error(util.inspect(error));
