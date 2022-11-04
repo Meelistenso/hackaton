92b41ef24a9c384e08d6983c1edc0a724fe7c2a9
Ihor Kovalenko <meelistenso@gmail.com>
2022-11-04 18:04:27 +0200

    Create pub sub provider

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PubSubService } from './providers/pubsub/pub-sub.service';
+import util from 'util';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  constructor(pubSubService: PubSubService) {
+    pubSubService.subscribe(
+      (message) => {
+        console.log(message);
+      },
+      (error) => {
+        console.error(util.inspect(error));
+        process.exit(1);
+      },
+    );
+  }
+
+  onModuleInit() {
+    console.log('On init');
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
